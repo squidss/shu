3909e94e4cfb4f00741e7db0c76541f29d9bf2ad
squid <690829513@qq.com>
2017-08-30 11:02:55 +0800

    更改
    更改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 		//'配置项'=>'配置值'
 		'URL_MODEL' => 2,
 		
-		/******** PDO专用定义，GAME测试专用 *******/
+		/******** PDO专用定义，测试用 *******/
 		'DB_TYPE' => 'mysql',
 		'DB_USER' => 'root',
 		'DB_PWD' => 'root',
